50462c281f592ad8508ddc21312e72614bfbe42d
Ato ARAKI <ato.araki@gmail.com>
2013-03-16 15:46:33 +0900

    Modified syslog level

--- syslog/main.go
+++ syslog/main.go
@@ -1,24 +1,24 @@
 package main
 
 import (
-	"log/syslog"
 	"log"
+	"log/syslog"
 )
 
 func main() {
-	l2, err := syslog.New(syslog.LOG_ERR, "GoExample")
-	defer l2.Close()
+	l3, err := syslog.New(syslog.LOG_ERR, "GoExample")
+	defer l3.Close()
 	if err != nil {
 		log.Fatal("error")
 	}
 
-    //l2.Emerg("emergency")
-	l2.Alert("alert")
-    l2.Crit("critical")
-    l2.Err("error")
-    l2.Warning("warning")
-    l2.Notice("notice")
-    l2.Info("information")
-    l2.Debug("debug")
-	l2.Write([]byte("write"))
+	//l3.Emerg("emergency")
+	l3.Alert("alert")
+	l3.Crit("critical")
+	l3.Err("error")
+	l3.Warning("warning")
+	l3.Notice("notice")
+	l3.Info("information")
+	l3.Debug("debug")
+	l3.Write([]byte("write"))
 }
